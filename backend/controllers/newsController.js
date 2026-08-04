1fbe578554d9f9840cff6474ffe77e851da67fad
greeng (K Nhân) <86702407+green96@users.noreply.github.com>
2026-08-04 22:38:11 +0700

    Thêm Chuan Code
    Thêm Chuan Code

--- backend/controllers/newsController.js
+++ backend/controllers/newsController.js
@@ -1,11 +1,20 @@
 const { crawlNews } = require("../services/newsCrawler");
 
+/**
+ * Lấy danh sách tin tức từ website CTUET.
+ *
+ * @param {Object} req
+ * @param {Object} res
+ * @returns {Promise<void>}
+ */
 exports.getNews = async (req, res) => {
     try {
         const news = await crawlNews();
+
         res.json(Array.isArray(news) ? news : []);
     } catch (error) {
         console.error("News fetch error:", error.message);
+
         res.json([]);
     }
 };
